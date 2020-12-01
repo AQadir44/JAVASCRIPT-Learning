68a00d9a244b618e08201b6bce3c5f4f33c66267
Aqsa Qadir <aqsa.qadir44@gmail.com>
2020-12-01 19:24:48 +0500

    javascript basic

---
+++ JAVASCRIPT-BASIC/main.js
@@ -0,0 +1,111 @@
+
+// console.log("hello world")
+
+// var a = "aqsa" //can change
+
+// let b ="aqsa" //can't change
+
+// const c = "aqsa" //can't change
+
+// console.log(a , b , c)
+
+// // now change the a b c
+// var a = "dur" // change 
+
+// //let b = "dur"
+
+// //const c =  "dur"
+
+// console.log(a , b , c)
+
+// var f = 5 % 3
+
+// console.log(f)
+
+// //increment and decrement 
+
+// var x = 10;
+
+// var d = x++ + ++x - x--
+
+// console.log(d)
+
+// //user input 
+
+// //string
+// var s = prompt("enter value" , "the value is")
+// //integar
+// var s = +prompt("enter value" , "the value is")
+
+// //marksheet task
+// var percentage = +prompt("Enter your Percentage: ");
+
+// if(percentage >= 80 ){
+//     console.log("A+ grade")
+// }
+
+// else if(percentage <80 && percentage >=70){
+//     console.log("A grade")
+// }
+
+// else if(percentage < 70 && percentage >=60){
+//     console.log("B grade")
+// }
+
+// else if(percentage < 60 && percentage >=50){
+//     console.log("C grade")
+// }
+
+// else if(percentage < 50 && percentage >=40){
+//     console.log("D grade")
+// }
+
+// else if(percentage < 40 ){
+//     console.log("fail")
+// }
+
+// //reverse word order and check reverse == original
+
+// var word = prompt("enter word");
+// var check = "";
+
+// for (var i = word.length -1 ; i >= 0 ; i--){
+//     check += word[i]
+// }
+
+// if (check === word){
+//     console.log(word + " is mirror word")
+// }
+
+// else{
+//     console.log(word + " is not a mirror word")
+// }
+
+
+// //calculator task
+
+// var var1=+prompt("Enter first value");
+// var var2=+prompt("Enter second value");
+// var sign = prompt("Enter operator");
+
+// if ( sign === "+"){
+//     console.log(var1+var2)
+// }
+// else if ( sign === "-"){
+//     console.log(var1-var2)
+// }
+
+// else if ( sign === "*"){
+//     console.log(var1*var2)
+// }
+
+// else if ( sign === "/"){
+//     console.log(var1/var2)
+// }
+
+// else if ( sign === ""){
+//     console.log(var1+var2)
+// }
+
+
+
